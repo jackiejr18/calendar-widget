19e647af200c4fca741ec6f5401779c25b1819e0
Jackie Jr <jwysinger@gmail.com>
2023-07-15 10:26:34 -0500

    added rest of html and some javescript

---
+++ calendarwidget.js
@@ -0,0 +1,14 @@
+//Sets weather and date values 
+$(document).ready(function) {
+	var ampm = true; // true or false
+	$.simpleWeather({
+		zipcode:'',
+		woeid: '2424766', // Houston, Texas
+		location: '',
+		unit: 'c',
+		success: function(weather) {
+
+			$()
+		}
+	})
+}
